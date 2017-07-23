4d3ffe6e8db870190dde5226db292a1357fb1da7
Bogdan Samondros <bogdan.samondros@gmail.com>
2017-07-23 14:10:49 +0300

    rename mod -> isModified

--- src/main/java/org/z1key/projects/sort/Bubble.java
+++ src/main/java/org/z1key/projects/sort/Bubble.java
@@ -5,17 +5,17 @@ public class Bubble {
     public static void sort(int[] array) {
         int length = array.length;
         for (int i = 0; i < array.length; i++) {
-            boolean mod = false;
+            boolean isModified = false;
             for (int k = 0; k < length - 1; k++) {
                 int memory;
                 if (array[k] > array[k + 1]) {
                     memory = array[k];
                     array[k] = array[k + 1];
                     array[k + 1] = memory;
-                    mod = true;
+                    isModified = true;
                 }
             }
-            if (!mod) break;
+            if (!isModified) break;
             length--;
         }
     }
